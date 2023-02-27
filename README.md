1c9ec2c8be42f3ff7717c8bde21c3f3f3e1ae7ae
SunKechang <1437821834@qq.com>
2023-02-27 16:18:15 +0800

    初始化

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # test_vue
 
-## Project setup
-```
-npm install
-```
+## 启动步骤
+1. npm install
+2. npm run serve
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## 详细说明
+首页有两个按钮，登录、写入数据。分别设计两个云函数：adminLogin和addOne。
 
-### Compiles and minifies for production
-```
-npm run build
-```
+因为这里主要模拟管理员后台登录和写入数据两个操作。所以把
 
-### Lints and fixes files
-```
-npm run lint
-```
+登录按钮会携带用户名、密码请求云函数adminLogin，获取ticket。得到ticket后调用微信的服务来获取uid。得到uid后就说明登录成功。具体操作参考：https://cloud.tencent.com/document/product/876/41731
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+另一个按钮用于向skc-test数据库写入数据，模拟管理员添加一条数据。在云函数界面点击“权限控制”，设置addOne函数为只有登录用户才能调用。
+![image](public/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20230227161413.png)
+
+然后客户端请求的时候header需要加上`x-cloudbase-credentials`字段，具体操作看参考。（参考：https://docs.cloudbase.net/service/authentication）
+
+剩下的数据库操作就在云函数里完成了。
+
+其中管理员登录用的**自定义登录**，有别于**微信登录**。
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,63 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="click">登录</button>
+    <button @click="write">写入数据</button>
   </div>
 </template>
 
 <script>
+import cloudbase from '@cloudbase/js-sdk';
+
+const app = cloudbase.init({
+  env: 'web-3gjrbbmi2da1e089'
+});
+
+const auth = app.auth({ persistence: 'none' });
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    async click() {
+      // 1. 建议登录前检查当前是否已经登录
+      if(!auth.hasLoginState()){
+        // 2. 请求开发者自有服务接口获取ticket
+        var url = "https://web-3gjrbbmi2da1e089-1316911075.ap-shanghai.app.tcloudbase.com/admin/login";
+        let ticket = await this.axios.get(url,{
+          params:{
+              'username': 'admin',
+              'password': 'admin'
+            }
+          }).then(function (res) {
+            return res.data
+          }).catch(function () {
+            return ""
+          });
+        if(ticket !== "") {
+          // 3. 登录 CloudBase
+          await auth.customAuthProvider().signIn(ticket);
+        }
+        
+      }
+    },
+    write() {
+      let authHeader = cloudbase.auth().getAuthHeader();
+      var url = "https://web-3gjrbbmi2da1e089-1316911075.ap-shanghai.app.tcloudbase.com/test/add";
+      this.axios.get(url, {
+        headers: {
+          ...authHeader
+        }
+      }).then(function (res) {
+        alert(res.data)
+      }).catch(function (err) {
+        alert(err)
+      });
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
